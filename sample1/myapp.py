dfd4d86b4ceba78930cf0f410b083cc26137afe0
gielo0111 <gielo0111@gmail.com>
2025-01-21 16:04:39 +1300

    body

--- sample1/myapp.py
+++ sample1/myapp.py
@@ -7,3 +7,24 @@ st.write("""
          
 Shown are the stock closing price and volume of Google!
 """)
+
+# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
+
+#define the ticker symbol
+tickerSymbol='GOOGL'
+
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+
+#get the historiucal prices for this sticker
+tickerDF = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+
+#open high low close bolume dividends stock splits
+st.write("""
+# Closing Price
+""")
+st.line_chart(tickerDF.Close)
+st.write("""
+# Volume Price
+""")
+st.line_chart(tickerDF.Volume)
